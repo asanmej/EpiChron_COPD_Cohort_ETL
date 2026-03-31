0c06194eea31f8c13887e04f9b9a82787a471dcb
Santos <98388993+asanmej@users.noreply.github.com>
2026-03-31 13:03:38 +0200

    Created main script and pathing to other scripts

---
+++ to_run.R
@@ -0,0 +1,21 @@
+# Author: Alejandro Santos Mejías
+# Date last update: 2026-03-30
+# Script name: to_run.R
+# Aims: To run all ETL process.
+
+rm(list = ls())
+gc()
+
+source("98_packages.R")
+
+source(past0(path_step, "Create_Patient_table.R"))
+
+source(past0(path_step, "Create_Visits_table.R"))
+
+source(past0(path_step, "Create_FFEV1_table.R"))
+
+source(past0(path_step, "Create_N_Exacerbaciones_table.R"))
+
+source(past0(path_step, "Create_Exacerbaciones_table.R"))
+
+source(past0(path_step, "Create_Enfermedades_table.R"))
